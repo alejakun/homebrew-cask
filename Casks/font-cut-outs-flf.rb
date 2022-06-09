1217530cc0ff22386cbee59ee634073c1465c960
Alex Pedroza <alejandro.pedroza@bwr.mx>
2022-06-09 10:22:44 -0500

    Reorganized gitignore

---
+++ Casks/font-cut-outs-flf.rb
@@ -0,0 +1,10 @@
+cask 'font-CutOutsFLF' do
+  version "1.0"
+  sha256 "d67a5660e8cdce42c0997e15fa5a15d2b656ff5e68e5b18341835b6b4a45bcc8"
+
+  url "https://#{ENV['HOMEBREW_GITHUB_API_TOKEN']}@raw.githubusercontent.com/alejakun/homebrew-cask-assets/master/Fonts/General/cut-outs-flf.zip"
+  name "CutOutsFLF"
+  homepage ""
+
+  font "CutOutsFLF.ttf"
+end
